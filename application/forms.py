dce0b0eacbb19a44805d03df07766879799aed61
Sophie Cosgrove <sophie.cosgrovee@hotmail.co.uk>
2020-05-17 19:44:23 +0000

    added update and delete functionality

--- application/forms.py
+++ application/forms.py
@@ -78,3 +78,65 @@ class CharacterForm(FlaskForm):
         if character:
             raise ValidationError('Character already logged')
 
+class UpdateEventForm(FlaskForm):
+    season = IntegerField('Season',
+            validators = [
+                DataRequired(),
+            ]
+        )
+    episode = IntegerField('Episode',
+            validators = [
+                DataRequired(),
+            ]
+        )
+    character = StringField('Character Name',
+            validators = [
+                DataRequired(),
+                Length(min=2, max=100)
+            ]
+        )
+    from_year = IntegerField('Travelled from',
+            validators = [
+                DataRequired(),
+            ]
+        )
+    to_year = IntegerField('Travelled to',
+            validators = [
+                DataRequired(),
+            ]
+        )
+    event = StringField('Event',
+            validators = [
+                DataRequired(),
+                Length(min=5, max=500)
+            ]
+        )
+    submit = SubmitField('Update Event')
+
+class UpdateCharacterForm(FlaskForm):
+    name = StringField('Name',
+            validators = [
+                DataRequired(),
+                ]
+            )
+    mother = StringField('Mother')
+    father = StringField('Father')
+    hair_colour = StringField('Hair Colour',
+            validators = [
+                DataRequired(),
+                Length(min=3, max=30)
+                ]
+            )
+    eye_colour = StringField('Eye Colour',
+            validators = [
+                DataRequired(),
+                Length(min=3, max=30)
+                ]
+            )
+    status = StringField('Alive or Deceased',
+            validators = [
+                DataRequired(),
+                Length(min=3, max=30)
+                ]
+            )
+    submit = SubmitField('Update Character')
--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,7 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from application import app, db
 from application.models import Events, Characters
-from application.forms import EventForm, CharacterForm
+from application.forms import EventForm, CharacterForm, UpdateEventForm, UpdateCharacterForm
 
 
 
@@ -39,7 +39,7 @@ def addevent():
         )
         db.session.add(eventData)
         db.session.commit()
-        flash('Your event has been added!')
+        flash('Your event has been added!', 'success')
         return redirect(url_for('eventlog'))
     else:
         print(form.errors)
@@ -60,9 +60,81 @@ def addcharacter():
                 )
         db.session.add(characterData)
         db.session.commit()
-        flash('Your character has been added!')
+        flash('Your character has been added!', 'success')
         return redirect(url_for('characters'))
     else:
         print(form.errors)
 
     return render_template('addcharacter.html', title='Add a Character', form=form)
+
+@app.route('/event/<int:event_id>')
+def event(event_id):
+    event = Events.query.get_or_404(event_id)
+    return render_template('event.html', title = event.id, event = event)
+
+@app.route('/event/<int:event_id>/update', methods=['GET', 'POST'])
+def update_event(event_id):
+    event = Events.query.get_or_404(event_id)
+    form = UpdateEventForm()
+    if form.validate_on_submit():
+        event.season = form.season.data
+        event.episode = form.episode.data
+        event.character = form.character.data
+        event.from_year = form.from_year.data
+        event.to_year = form.to_year.data
+        event.event = form.event.data
+        db.session.commit()
+        flash('Event has been updated!', 'success')
+        return redirect(url_for('event', event_id=event.id))
+    elif request.method == 'GET':
+        form.season.data = event.season
+        form.episode.data = event.episode
+        form.character.data = event.character
+        form.from_year.data = event.from_year
+        form.to_year.data = event.to_year
+        form.event.data = event.event
+    return render_template('addevent.html', title = 'Update Event', form = form)
+
+@app.route("/event/<int:event_id>/delete", methods=['POST'])
+def delete_event(event_id):
+    event = Events.query.get_or_404(event_id)
+    db.session.delete(event)
+    db.session.commit()
+    flash('Event has been deleted!', 'success')
+    return redirect(url_for('eventlog'))
+
+@app.route('/character/<int:character_id>')
+def character(character_id):
+    character = Characters.query.get_or_404(character_id)
+    return render_template('character.html', title = character.id, character = character)
+
+@app.route('/character/<int:character_id>/update', methods=['GET', 'POST'])
+def update_character(character_id):
+    character = Characters.query.get_or_404(character_id)
+    form = UpdateCharacterForm()
+    if form.validate_on_submit():
+        character.name = form.name.data
+        character.mother = form.mother.data
+        character.father = form.father.data
+        character.hair_colour = form.hair_colour.data
+        character.eye_colour = form.eye_colour.data
+        character.status = form.status.data
+        db.session.commit()
+        flash('Character has been updated!', 'success')
+        return redirect(url_for('character', character_id=character.id))
+    elif request.method == 'GET':
+        form.name.data = character.name
+        form.mother.data = character.mother
+        form.father.data = character.father
+        form.hair_colour.data = character.hair_colour
+        form.eye_colour.data = character.eye_colour
+        form.status.data = character.status
+    return render_template('addcharacter.html', title = 'Update Character', form = form)
+
+@app.route("/character/<int:character_id>/delete", methods=['POST'])
+def delete_character(character_id):
+    character = Characters.query.get_or_404(character_id)
+    db.session.delete(character)
+    db.session.commit()
+    flash('Character  has been deleted!', 'success')
+    return redirect(url_for('characters'))
