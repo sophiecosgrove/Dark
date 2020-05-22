ecb02d31d4ac57b7ef3ec740c484f613a9ae240e
Sophie Cosgrove <sophie.cosgrovee@hotmail.co.uk>
2020-05-22 11:34:19 +0000

    changed installation

--- script/installation.sh
+++ script/installation.sh
@@ -16,4 +16,4 @@ pip3 install -r requirements.txt
 
 source ~/.bashrc
 
-gunicorn --workers=4 --bind=0.0.0.0:5000 /var/lib/jenkins/workspace/dark_freestyle/app:app
+gunicorn --workers=4 --bind=0.0.0.0:5000 /var/lib/jenkins/workspace/dark_freestyle/application:app
